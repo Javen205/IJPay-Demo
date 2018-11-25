df0c145e2020bdf6917caa606495041a6f8748dc
Javen205 <javen205@126.com>
2018-11-25 14:58:49 +0800

    update something

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.ijpay.config.StartupRunner;
 import com.ijpay.config.TaskRunner;
+import com.jfinal.template.ext.spring.JFinalViewResolver;
 
 @SpringBootApplication
 @EnableCaching
@@ -15,14 +16,27 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	 @Bean
-     public StartupRunner startupRunner(){
-         return new StartupRunner();
-     }
 
-     @Bean
-     public TaskRunner taskRunner(){
-         return new TaskRunner();
-     }
+	@Bean
+	public StartupRunner startupRunner() {
+		return new StartupRunner();
+	}
+
+	@Bean
+	public TaskRunner taskRunner() {
+		return new TaskRunner();
+	}
+
+	@Bean(value = "jfinalViewResolver")
+	public JFinalViewResolver getJFinalViewResolver() {
+		JFinalViewResolver jf = new JFinalViewResolver();
+		jf.setDevMode(true);
+		jf.setCache(false);
+		// jf.setSourceFactory(new ClassPathSourceFactory());
+		jf.setPrefix("/WEB-INF/_views/");
+		// jf.setSuffix(".html");
+		jf.setContentType("text/html;charset=UTF-8");
+		jf.setOrder(0);
+		return jf;
+	}
 }
--- src/main/java/com/ijpay/config/WebMvcConfigurer.java
+++ src/main/java/com/ijpay/config/WebMvcConfigurer.java
@@ -1,15 +1,20 @@
 package com.ijpay.config;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.ijpay.interceptor.AliPayInterceptor;
 import com.ijpay.interceptor.CharacterEncodInterceptor;
 import com.ijpay.interceptor.WxPayInterceptor;
@@ -27,6 +32,31 @@ public class WebMvcConfigurer extends WebMvcConfigurationSupport {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         super.configureMessageConverters(converters);
+        
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(
+
+                SerializerFeature.WriteMapNullValue,// 保留map空的字段
+
+                SerializerFeature.WriteNullStringAsEmpty, // 将String类型的null转成""
+
+                SerializerFeature.WriteNullNumberAsZero,// 将Number类型的null转成0
+
+                SerializerFeature.WriteNullListAsEmpty,// 将List类型的null转成[]
+
+                SerializerFeature.WriteNullBooleanAsFalse, // 将Boolean类型的null转成false
+
+                SerializerFeature.DisableCircularReferenceDetect);// 避免循环引用
+
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(Charset.forName("UTF-8"));
+        List<MediaType> mediaTypeList = new ArrayList<MediaType>();
+        // 解决中文乱码问题，相当于在Controller上的@RequestMapping中加了个属性produces = "application/json"
+        mediaTypeList.add(MediaType.APPLICATION_JSON);
+        converter.setSupportedMediaTypes(mediaTypeList);
+        converters.add(converter);
+        
         converters.add(responseBodyConverter());
     }
     
