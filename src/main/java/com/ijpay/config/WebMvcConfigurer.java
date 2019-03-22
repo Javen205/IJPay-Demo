af26e03c281572e93874dc2e931902c237d35c39
Javen205 <javen205@126.com>
2019-03-22 22:25:09 +0800

    解决静态文件无法显示的问题

--- src/main/java/com/ijpay/config/WebMvcConfigurer.java
+++ src/main/java/com/ijpay/config/WebMvcConfigurer.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
@@ -29,11 +30,11 @@ public class WebMvcConfigurer extends WebMvcConfigurationSupport {
 		super.addInterceptors(registry);
 	}
 
-//	@Override
-//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		// 将所有/static/** 访问都映射到classpath:/static/ 目录下
-//		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-//	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 将所有/static/** 访问都映射到classpath:/static/ 目录下
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
 
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
