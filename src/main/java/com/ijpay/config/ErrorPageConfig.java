11e550d627b7d9b5df08658d08ed09e3c0b75b81
Javen205 <javen205@126.com>
2019-05-12 10:39:23 +0800

    40x、50x 页面处理

--- src/main/java/com/ijpay/config/ErrorPageConfig.java
+++ src/main/java/com/ijpay/config/ErrorPageConfig.java
@@ -11,9 +11,9 @@ public class ErrorPageConfig implements ErrorPageRegistrar{
 
 	@Override
 	public void registerErrorPages(ErrorPageRegistry registry) {
-		ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401.html");
-		ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html");
-		ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500.html");
+		ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/static/error/401.html");
+		ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/static/error/404.html");
+		ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/static/error/500.html");
 		registry.addErrorPages(error401Page,error404Page,error500Page);
 	}
 
